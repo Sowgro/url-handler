7f8d16eb6d5b1813445ff467ddcf37c3e7e8e9ed
sowgro <tpoke.ferrari@gmail.com>
2026-05-02 22:19:07 -0400

    2

--- app.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-url = sys.argv[1]
-
-if url.startswith('https://open.spotify.com/'):
-    os.system(f'flatpak run com.spotify.Client {url} &')
-else:
-    os.system(f'firefox {url} &')
---
+++ url-handler.py
@@ -0,0 +1,22 @@
+import json
+import os
+import sys
+from types import SimpleNamespace
+
+url = sys.argv[1] if len(sys.argv) else ""
+config = json.load(open('config.json'), object_hook=lambda d: SimpleNamespace(**d))
+
+def getMatcher(handler: str):
+    match handler:
+        case 'contains' : return lambda a, b: b in a
+        case 'startswith': return lambda a, b: a.startswith(b)
+        case 'endswith'  : return lambda a, b: a.endswith(b)
+
+def getApp():
+    for item in config.handlers:
+        if getMatcher(item.matcher)(url, item.string):
+            return item.exec
+    return config.default.exec
+exec: str = getApp()
+
+os.system(exec.format(url))
